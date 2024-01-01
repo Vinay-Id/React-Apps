f3e3ba2841afecad33a8aa6fa0b12c19ac38f076
Vinay Vishwakarma <vnypro98@gmail.com>
2024-01-01 22:12:43 +0530

    form component added

--- form-App/src/components/Form.jsx
+++ form-App/src/components/Form.jsx
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 const Form = () => {
+  const [data, setData] = useState({ username: "", password: "" });
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((prev) => ({ ...prev, [name]: value }));
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(data);
+    setData({ username: "", password: "" });
+  }
   return (
     <div className="container">
-      <form>
-        <input type="text" placeholder="User Name" />
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="User Name"
+          name="username"
+          onChange={handleChange}
+          value={data.username}
+        />
         <br />
-        <input type="password" placeholder="password" name="" id="" />
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          onChange={handleChange}
+          value={data.password}
+        />
         <br />
         <button>submit</button>
       </form>
