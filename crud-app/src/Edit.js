b79db5dcc38af09b5d74d64806a4019d16910d61
vinay vishwakarma <vnypro98@gmail.com>
2024-09-07 21:16:02 +0530

    made few changes in crud app

--- crud-app/src/Edit.js
+++ crud-app/src/Edit.js
@@ -1,35 +1,54 @@
-import { useEffect,useState } from "react"
-import axios from "axios"
-import { useParams,useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams, useNavigate } from "react-router-dom";
+import Form from "react-bootstrap/Form";
+// import Button from "react-bootstrap/Form";
 
 function Edit() {
-    const {id}=useParams();
-    const [user,setUser]=useState('');
-    const navigate=useNavigate()
+  const { id } = useParams();
+  const [user, setUser] = useState("");
+  const navigate = useNavigate();
 
-    useEffect(()=>{
-        axios.get(`https://jsonplaceholder.typicode.com/users/${id}`).then((response)=>setUser(response.data.name)).catch(error=>console.error('error occured while editing','error'))
-    },[id]);
-    const handleSubmit=(e)=>{
-        e.preventDefault();
-        axios.put(`https://jsonplaceholder.typicode.com/users/${id}`).then( ()=> navigate('/')).catch(error=>console.error('error occured while editing','error'))
-    }
-    console.log(user);
-    
+  useEffect(() => {
+    axios
+      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => setUser(response.data.name))
+      .catch((error) => console.error("error occured while editing", "error"));
+  }, [id]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then(() => navigate("/"))
+      .catch((error) => console.error("error occured while editing", "error"));
+  };
+  console.log(user);
+
   return (
-    <div className='bg-gradient screen-size d-flex-content'>
-      <div  className='card'>
+    <div className="bg-gradient screen-size d-flex-content">
+      <div className="card">
         <h2>Edit user</h2>
-        <br/>
-         <form onSubmit={handleSubmit}>
-        <label>
+        <br />
+        <form onSubmit={handleSubmit}>
+          {/* <label>
           Name: <input type="text" value={user} onChange={(e)=>setUser(e.target.value)} />
-        </label>
-        <button type="submit">Update</button>
-      </form>
+        </label> */}
+
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="name"
+              value={user}
+              onChange={(e) => setUser(e.target.value)}
+            />
+          </Form.Group>
+          {/* <Button as="input" type="submit" value="Submit">Primary</Button>{' '} */}
+          <button type="submit">Update</button>
+        </form>
       </div>
     </div>
-  )
+  );
 }
 
-export default Edit
+export default Edit;
